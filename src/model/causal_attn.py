421ba9e5369c56e9d660f262a05b45d687d393d3
Manoj-Telrandhe <your-email@example.com>
2026-01-08 17:39:18 +0530

    FeedForward NN

--- src/model/causal_attn.py
+++ src/model/causal_attn.py
@@ -5,8 +5,7 @@ import torch.nn as nn
 
 class CausalAttention(nn.Module):
 
-    def __init__(self, d_in, d_out, context_length,
-                 dropout, qkv_bias=False):
+    def __init__(self, d_in, d_out, context_length, dropout, qkv_bias=False):
         super().__init__()
         self.d_out = d_out
         self.W_query = nn.Linear(d_in, d_out, bias=qkv_bias)
