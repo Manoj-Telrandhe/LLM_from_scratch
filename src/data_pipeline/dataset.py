667de422fc059433e794d92dbf0d461e4b94390f
Manoj-Telrandhe <your-email@example.com>
2026-01-09 18:44:33 +0530

    create input-target pairs using sliding window

--- src/data_pipeline/dataset.py
+++ src/data_pipeline/dataset.py
@@ -27,26 +27,3 @@ class GPTDataset(Dataset):
         return self.input_ids[idx], self.target_ids[idx]
 
 
-def create_dataloader(
-    txt,
-    batch_size=4,
-    max_length=256,
-    stride=128,
-    shuffle=True,
-    drop_last=True,
-    num_workers=0,
-):
-    
-    tokenizer = my_tokenizer()
-
-    dataset = GPTDataset(txt, tokenizer, max_length, stride)
-
-    dataloader = DataLoader(
-        dataset,
-        batch_size=batch_size,
-        shuffle=shuffle,
-        drop_last=drop_last,
-        num_workers=num_workers,
-    )
-
-    return dataloader
