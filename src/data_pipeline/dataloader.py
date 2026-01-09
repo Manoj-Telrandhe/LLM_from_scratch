fe8c5e807c2c55d61bf29ddbaa80c9e290e33294
Manoj-Telrandhe <your-email@example.com>
2026-01-09 18:43:09 +0530

    dataloader function

---
+++ src/data_pipeline/dataloader.py
@@ -0,0 +1,29 @@
+import torch
+from torch.utils.data import DataLoader
+from src.data_pipeline.dataset import GPTDataset
+from src.tokenizer.bpe_tokenizer import my_tokenizer
+
+def create_dataloader(
+    txt,
+    batch_size,
+    max_length,
+    stride,
+    shuffle=True,
+    drop_last=True,
+    num_workers=0,
+):
+    
+    tokenizer = my_tokenizer()
+
+    dataset = GPTDataset(txt, tokenizer, max_length, stride)
+
+
+    dataloader = DataLoader(
+        dataset,
+        batch_size=batch_size,
+        shuffle=shuffle,
+        drop_last=drop_last,
+        num_workers=num_workers,
+    )
+
+    return dataloader
